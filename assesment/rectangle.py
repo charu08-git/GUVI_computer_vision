25e123cd01bc0103a759024ba09ea3e6b88965eb
charu08-git <charuhasini008@gmail.com>
2025-08-22 11:50:41 +0530

    Add files via upload

---
+++ assesment/rectangle.py
@@ -0,0 +1,9 @@
+import cv2
+import matplotlib.pyplot as plt
+img=cv2.imread("image.jpeg")
+print(img.shape)
+cv2.rectangle(img,(50,50),(100,100),(255,0,0),5)
+resize=cv2.resize(img,(2000,2000))
+plt.imshow(cv2.cvtColor(resize,cv2.COLOR_BGR2RGB))
+plt.axis("off")
+plt.show()
